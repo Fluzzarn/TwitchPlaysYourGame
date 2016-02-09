f28c282a4cbcc94afed87226bd1e904c09546b46
fluzzarn <fluzzarn@gmail.com>
2016-02-08 20:16:40 -0500

    added unity game exmaple

---
+++ UnityGame/TwitchPlaysDemoGame/Assets/Card.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+    public int ID;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
